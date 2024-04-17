c3186129b94b25b8f6cf02e1af7c891392a6df74
Caleb_Lykken <Lykken@uw.edu>
2024-04-17 14:09:37 -0700

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-
+function app () {
+    return (
+        <div>
+            <h1>Sunrise</h1>
+        </div>
+    );
+}
+export default app;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdFgsZoP0rBiaKPAbHwyO_N_vLJNfJa1A",
+  authDomain: "sunrise-5c9b5.firebaseapp.com",
+  projectId: "sunrise-5c9b5",
+  storageBucket: "sunrise-5c9b5.appspot.com",
+  messagingSenderId: "753187618836",
+  appId: "1:753187618836:web:3f55910ef55c53ced96701"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
