a02d532a7b8e9c3c11885ea3b401bb24496ab116
JunnaC <junnacao@163.com>
2024-05-15 12:47:29 -0700

    resource pages created

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,17 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './navbar.css';
-function navbar () {
-  return (
-      <div class = "navbar">
-          <div class ="logo">
-            <img src="img/logo.png" className="logo-icon" />
-          </div>
-          <div class = "col text-left">
-            <button type ="button" className = "btn"><div className = 'nav-text'>Quiz</div></button>
-            <button type ="button" className = "btn"><div className = 'nav-text'>Diagram</div></button>
-            <button type ="button" className = "btn"><div className = 'nav-text'>Recources</div></button>
-          </div>
 
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <NavLink to="/">
+          <img src="img/logo.png" className="logo-icon" alt="Logo" />
+        </NavLink>
       </div>
+      <div className="col text-left">
+        <NavLink to="/quiz" className="btn"><div className="nav-text">Quiz</div></NavLink>
+        <NavLink to="/diagram" className="btn"><div className="nav-text">Diagram</div></NavLink>
+        <NavLink to="/resources" className="btn"><div className="nav-text">Resources</div></NavLink>
+      </div>
+    </div>
   );
 }
-export default navbar;
+
+export default Navbar;
